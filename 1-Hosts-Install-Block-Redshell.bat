1a55e03ce5fadd483f7e2d51032524e27631a36e
Chromos-Def <dewp20x@gmail.com>
2019-03-03 21:22:45 -0500

    Fixed the hosts batch files

--- 1-Hosts-Install-Block-Redshell.bat
+++ 1-Hosts-Install-Block-Redshell.bat
@@ -7,28 +7,13 @@ echo.
 echo.>> %hostspath%
 echo #Start of Block Redshell Spyware Entries# >> %hostspath%
 echo 0.0.0.0 redshell.io >> %hostspath%
-echo 0.0.0.0 138.91.197.151 >> %hostspath%
 echo 0.0.0.0 api.redshell.io >> %hostspath%
-echo 0.0.0.0 40.118.187.121 >> %hostspath%
 echo 0.0.0.0 treasuredata.com >> %hostspath%
-echo 0.0.0.0 23.185.0.2 >> %hostspath%
 echo 0.0.0.0 api.treasuredata.com >> %hostspath%
-echo 0.0.0.0 34.194.99.1 >> %hostspath%
-echo 0.0.0.0 52.3.23.198 >> %hostspath%
-echo 0.0.0.0 54.80.12.77 >> %hostspath%
 echo 0.0.0.0 in.treasuredata.com >> %hostspath%
-echo 0.0.0.0 34.199.53.125 >> %hostspath%
-echo 0.0.0.0 50.17.28.69 >> %hostspath%
-echo 0.0.0.0 52.0.125.186 >> %hostspath%
-echo 0.0.0.0 34.200.80.85 >> %hostspath%
-echo 0.0.0.0 34.202.72.93 >> %hostspath%
-echo 0.0.0.0 54.174.193.179 >> %hostspath%
 echo 0.0.0.0 cdn.rdshll.com >> %hostspath%
 echo 0.0.0.0 t.redshell.io >> %hostspath%
-echo 0.0.0.0 138.91.197.151 >> %hostspath%
 echo 0.0.0.0 innervate.us >> %hostspath%
-echo 0.0.0.0 40.71.103.150 >> %hostspath%
-echo 0.0.0.0 40.121.210.226 >> %hostspath%
 echo #End of Block Redshell Spyware Entries# >> %hostspath%
 echo.>> %hostspath%
 echo.
